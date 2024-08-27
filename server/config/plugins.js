b94e904ea8a5aff0bac762285b8fa3376e97a0a6
Attila <hajaattila@gmail.com>
2024-08-27 11:18:19 +0200

    [MAJOR] server, nodemailer setup

--- server/config/plugins.js
+++ server/config/plugins.js
@@ -1,19 +1,21 @@
 module.exports = ({ env }) => ({
     email: {
-      provider: "nodemailer",
-      providerOptions: {
-        host: "smtp.rackhost.hu",
-        port: 465,
-        secure: true, // A secure true értéke szükséges lehet az SSL/TLS-hez a 465-ös porton
-        auth: {
-          user: env("EMAIL_USER", "dev@bytemates.hu"),
-          pass: env("EMAIL_PASS", "karolyi423"),
+        config: {
+            provider: "nodemailer",
+            providerOptions: {
+                host: "smtp.rackhost.hu",
+                port: 465,
+                auth: {
+                    user: env("EMAIL_USER", "dev@bytemates.hu"),
+                    pass: env("EMAIL_PASS", "karolyi423"),
+                },
+                logger: true,
+                debug: true,
+            },
+            settings: {
+                defaultFrom: "dev@bytemates.hu",
+                defaultReplyTo: "dev@bytemates.hu",
+            },
         },
-      },
-      settings: {
-        defaultFrom: "dev@bytemates.hu",
-        defaultReplyTo: "dev@bytemates.hu",
-      },
     },
-  });
-  
+});
