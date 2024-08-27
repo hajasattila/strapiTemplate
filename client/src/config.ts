7de5c9fb78a2bb32b74a92f651c98fe24d486c34
Attila <hajaattila@gmail.com>
2024-08-27 11:00:48 +0200

    [MAJOR] basic structure

---
+++ client/src/config.ts
@@ -0,0 +1,4 @@
+export const config = {
+    SITENAME: 'Reset-Viki',
+    STRAPI: 'http://localhost:1337',
+};
--- server/config/plugins.js
+++ server/config/plugins.js
@@ -1,1 +1,19 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    email: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: "smtp.rackhost.hu",
+        port: 465,
+        secure: true, // A secure true értéke szükséges lehet az SSL/TLS-hez a 465-ös porton
+        auth: {
+          user: env("EMAIL_USER", "dev@bytemates.hu"),
+          pass: env("EMAIL_PASS", "karolyi423"),
+        },
+      },
+      settings: {
+        defaultFrom: "dev@bytemates.hu",
+        defaultReplyTo: "dev@bytemates.hu",
+      },
+    },
+  });
+  
